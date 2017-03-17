73e39eeb37de98b9652a637daee66cf39e230011
alexandremstf <alexandremstf@gmail.com>
2017-03-17 15:45:08 -0300

    Controle

--- files/arduino/control_matlab/control_matlab.ino
+++ files/arduino/control_matlab/control_matlab.ino
@@ -8,37 +8,33 @@ const byte BIN1 = 9;
 const byte BIN2 = 10;
 const byte PWM_MOTOR2 = 11;
 
-unsigned long left_border_millis = 0;
-unsigned long left_last_border_millis = 0;
-unsigned long right_border_millis = 0;
-unsigned long right_last_border_millis = 0;
+unsigned long left_border_micros = 0, right_border_micros = 0;
+unsigned long left_last_border_micros = 0, right_last_border_micros = 0;
 
 const float controler_frequency = 150; // Hz
-const float dt = 1 / controler_frequency;
-const float speed_calc = ( (2 * PI * 1.6) / 375 ) / 0.000001; // 1.6 -> raio | 375 -> leituras encoder | 0.000001 -> ajuste de microsegundo para segundo
+const float dt = 1 / controler_frequency; // seconds
+const float speed_calc = ( (2 * PI * 1.6) / 375 ) / 0.000001; // radius, encoder readings, seconds
 
-int left_array[5] = {0,0,0,0,0};
-int right_array[5] = {0,0,0,0,0};
-int speed_left = 0;
-int speed_right = 0;
-int pwm_left = 0;
-int pwm_right = 0;
+int left_pwm = 0, right_pwm = 0;
+int left_speed = 0, right_speed = 0;
+int left_array[5] = {0,0,0,0,0}, right_array[5] = {0,0,0,0,0};
 
-double error_prior_left = 0, error_prior_right = 0;
-double integral_left = 0, integral_right = 0;
+// PID variables 
 double KP = 5.3, KI = 220, KD = 0;
+double left_integral = 0, right_integral = 0;
+double left_error_prior = 0, right_error_prior = 0;
 
 unsigned long fps_time = 0;
 int cont = 0;
 
 void setup() {
-  pinMode(PWM_MOTOR1, OUTPUT);//PWMA
-  pinMode(AIN2, OUTPUT);//AIN2
-  pinMode(AIN1, OUTPUT);//AIN1
-  pinMode(STBY, OUTPUT);//STBY
-  pinMode(BIN1, OUTPUT);//BIN1
-  pinMode(BIN2, OUTPUT);//BIN2
-  pinMode(PWM_MOTOR2, OUTPUT);//PWMB
+  pinMode(PWM_MOTOR1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWM_MOTOR2, OUTPUT);
 
   Serial.begin(115200); 
 
@@ -63,14 +59,31 @@ void loop() {
   forward();
 
   /*
-  pwm_left = 100;
-  analogWrite(PWM_MOTOR2, pwm_left);
+  left_pwm = 100;
+  analogWrite(PWM_MOTOR2, left_pwm);
   delay(1000);
   
-  pwm_left = 255;
-  analogWrite(PWM_MOTOR2, pwm_left);
+  left_pwm = 255;
+  analogWrite(PWM_MOTOR2, left_pwm);
   delay(1000);
   */
+}
+
+ISR(TIMER1_COMPA_vect) {
+  
+  left_pwm = PID(70, left_speed, &left_integral, &left_error_prior);
+  analogWrite(PWM_MOTOR2, left_pwm);
+  
+  right_pwm = PID(70, right_speed, &right_integral, &right_error_prior);    
+  analogWrite(PWM_MOTOR1, right_pwm);
+  
+  Serial.print(left_speed);
+  Serial.print("\t");
+  Serial.print(left_pwm);
+  Serial.print("\t");
+  Serial.print(right_speed);
+  Serial.print("\t");
+  Serial.println(right_pwm);
 }
 
 int PID(double setpoint, double input, double *integral, double *error_prior){
@@ -91,53 +104,23 @@ int PID(double setpoint, double input, double *integral, double *error_prior){
 }
 
 void encoderLeft() {
-  left_border_millis = micros() - left_last_border_millis;
-  left_last_border_millis = micros();
-  speed_left = speedAverage(speed_calc / left_border_millis, left_array);
+  left_border_micros = micros() - left_last_border_micros;
+  left_last_border_micros = micros();
+  left_speed = speedAverage(speed_calc / left_border_micros, left_array);
 }
 
 void encoderRight() {
-  right_border_millis = micros() - right_last_border_millis;
-  right_last_border_millis = micros();
-  speed_right = speedAverage(speed_calc / right_border_millis, right_array);
-}
-
-void configTimerControler(){
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1  = 0;
-  OCR1A = (16000000/1024/controler_frequency) - 1;
-  TCCR1B |= (1 << WGM12);
-  TCCR1B |= (1 << CS12);   
-  TCCR1B |= (1 << CS10);  
-  TIMSK1 |= (1 << OCIE1A);
-}
-
-ISR(TIMER1_COMPA_vect) {
-  
-  pwm_left = PID(70, speed_left, &integral_left, &error_prior_left);
-  analogWrite(PWM_MOTOR2, pwm_left);
-  
-  pwm_right = PID(70, speed_right, &integral_right, &error_prior_right);    
-  analogWrite(PWM_MOTOR1, pwm_right);
-  
-  Serial.print(speed_left);
-  Serial.print("\t");
-  Serial.print(pwm_left);
-  Serial.print("\t");
-  Serial.print(speed_right);
-  Serial.print("\t");
-  Serial.println(pwm_right);
+  right_border_micros = micros() - right_last_border_micros;
+  right_last_border_micros = micros();
+  right_speed = speedAverage(speed_calc / right_border_micros, right_array);
 }
 
 int speedAverage(int num, int *array){
-  
   array[0] = array[1];
   array[1] = array[2];
   array[2] = array[3];
   array[3] = array[4];
   array[4] = num;
-    
   return (array[0] + array[1] + array[2] + array[3] + array[4]) / 5;
 }
 
@@ -175,4 +158,15 @@ void turnLeft() {
 
 void stopped() {
   digitalWrite(STBY, LOW);
+}
+
+void configTimerControler(){
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = (16000000/1024/controler_frequency) - 1;
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS12);   
+  TCCR1B |= (1 << CS10);  
+  TIMSK1 |= (1 << OCIE1A);
 }
