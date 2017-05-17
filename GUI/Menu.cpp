734b0fadf5d51359400044dbb0c6a1e7af4fa88c
alexandremstf <alexandremstf@gmail.com>
2017-05-17 01:40:02 -0300

    Glade

--- GUI/Menu.cpp
+++ GUI/Menu.cpp
@@ -9,41 +9,37 @@ int Menu::GUI(){
 			
 	app = Gtk::Application::create();
 
-	Gtk::Window window;	
-		window.set_default_size(800, 600);
-		window.set_icon_from_file("files/images/logo-rodetas.png");
-		window.set_position(Gtk::WIN_POS_CENTER_ALWAYS);
-		window.set_title("Rodetas");
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("GUI/Menu.glade");
 
-	Gtk::Button button_play, button_calibration, button_simulator, button_arduino, button_exit;
-		button_play.add_label("Play");
-		button_calibration.add_label("Calibration");
-		button_simulator.add_label("Simulator");
-		button_arduino.add_label("Arduino");
-		button_exit.add_label("Exit");
-
-		//button_play.set_size_request(200, 20);
-		button_play.signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonPlay) );
-		button_calibration.signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonCalibration) );
-		button_simulator.signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonSimulator) );
-		button_arduino.signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonArduino) );
-		button_exit.signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonExit) );
-
-	Gtk::ButtonBox button_box(Gtk::ORIENTATION_VERTICAL);
-		button_box.set_layout(Gtk::BUTTONBOX_CENTER );
-		button_box.set_spacing(20);
-		button_box.pack_start(button_play);
-		button_box.pack_start(button_calibration);
-		button_box.pack_start(button_simulator);
-		button_box.pack_start(button_arduino);
-		button_box.pack_start(button_exit);
-		button_box.show_all();
-
-	window.add(button_box);
-
-  	app->run(window);
+	Gtk::Window* window = nullptr;
+	Gtk::Button* button_play = nullptr;
+	Gtk::Button* button_calibration = nullptr;
+	Gtk::Button* button_simulator = nullptr;
+	Gtk::Button* button_arduino = nullptr;
+	Gtk::Button* button_exit = nullptr;
 	
-	//window.close();
+	builder->get_widget("Window Menu", window);
+	builder->get_widget("Button Play", button_play);
+	builder->get_widget("Button Calibration", button_calibration);
+	builder->get_widget("Button Simulator", button_simulator);
+	builder->get_widget("Button Arduino", button_arduino);
+	builder->get_widget("Button Exit", button_exit);
+
+	button_play->signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonPlay) );
+	button_calibration->signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonCalibration) );
+	button_simulator->signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonSimulator) );
+	button_arduino->signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonArduino) );
+	button_exit->signal_clicked().connect( sigc::mem_fun(this, &Menu::onButtonExit) );
+	
+	app->run(*window);
+
+	delete window;
+	delete button_play;
+	delete button_calibration;
+	delete button_simulator;
+	delete button_arduino;
+	delete button_exit;	
+
 	return program_state;
 } 
 
